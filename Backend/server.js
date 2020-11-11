27d181a51e9095f7e4c01244cbf678c0c413059d
Andrew Fauble <andrew.fauble@gmail.com>
2020-11-11 15:08:22 -0500

    cleaned up server.js and .env files they properly connect to the mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,44 +1,41 @@
-// Using the objects below!
-const express = require('express');// middle-wear
-const bodyParser = require('body-parser');// parsing the html
+// Run 'nodemon server' or 'npm start'
+const express = require('express'); // middle-wear
 const cors = require('cors');
 const mongoose = require('mongoose');
-//const { string } = require('prop-types');
-//const { SourceMap } = require('module');
+
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
+const port = process.env.PORT;
+const uri = process.env.ATLAS_URI;
+console.log(uri);
 
-//*************** CONNECTING TO MONGODB **********/
-//const MongoClient = require('mongodb').MongoClient;
-//password Popgame1
-const url = 'mongodb+srv://LargeProject:Popgame1@cluster0.l32tu.mongodb.net/<dbname>?retryWrites=true&w=majority';
-//const client = new MongoClient(url);
-//client.connect({useNewUrlParser : true, useUnifiedTopology : true});
-//*************** CONNECTION TO MONGO DB ENDED *****************/
-
-//********* Mongoose connection ********/
-mongoose.connect(url || 'mongodb://localhost/LargeProject',{useNewUrlParser : true, useUnifiedTopology : true});
-
-mongoose.connection.on('connected', () => {
-    console.log('Mongoose is connected!!!');
-});
-//***********Mongoose connection end******/
-
-app.use((req, res, next) =>
-{
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
+// connecting ot mongoose
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+// checking connection
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
 });
 
-app.listen(5000); // start Node + Express server on port 5000
+// start Node + Express server on port
+app.listen(port, () => {
+  console.log('Server is running on port: ' + port);
+});
+
+//NOTE: Code from professor for connection to web server
+// app.use((req, res, next) =>
+// {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, PATCH, DELETE, OPTIONS'
+//   );
+//   next();
+// });
