9af8cadd4d1179e99431c093d2bcbb81b71d023f
EddBoyo <edd.baez42@knights.ucf.edu>
2020-11-13 15:58:19 -0500

    schedule builder mostly finished

--- Frontend/mern-react/src/components/ScheduleBuildPage/ScheduleBuilder.js
+++ Frontend/mern-react/src/components/ScheduleBuildPage/ScheduleBuilder.js
@@ -8,7 +8,9 @@ import 'react-datepicker/dist/react-datepicker.css';
 const options = [
     'one', 'two'
 ];
-
+var count = 0;
+var timeArray = [];
+var dateArray = [];
 
 function ScheduleBuilder()
 {
@@ -23,18 +25,65 @@ function ScheduleBuilder()
     
     function submitBlock()
     {
-        alert('submitButton pressed ' + value.value + ' ' + startDate.toDateString());
+        if (count === 0)
+        {
+            alert('Please add atleast 1 time Slot');
+            return;
+        }
+        alert('submitButton pressed ' + timeArray[0] + ' ' + dateArray[0] + ' ' +timeArray.length);
+    }
+
+    function addText()
+    {
+        timeArray.push(value.value);
+        dateArray.push(startDate.toDateString());
+        var newdiv = document.createElement('div');
+        newdiv.innerHTML = ('<div id = "form'+(count)+'"><span id="innerPart">Time Slot '+(count + 1)+':<text id="timeText">{'+dateArray[count]+'}</text>'+'<text id="dateText">{'+timeArray[(count)]+'}</text>'+' <br /></span>' + '</div>');
+        if (count === 0)
+        {
+            document.getElementById("form").appendChild(newdiv);
+        }
+        else
+        {
+            document.getElementById("form"+(count - 1)).appendChild(newdiv);
+        }
+        count++;
+        return;
+    }
+
+    function deleteText()
+    {
+        if (count === 0)
+        {
+            return;
+        }
+        var div = document.getElementById("form"+(count - 1));
+        div.parentNode.removeChild(div);
+        timeArray.splice((count - 1), 1);
+        dateArray.splice((count - 1), 1);
+        count--;
+        return;
     }
 
     return(
         <div id="ScheduleDiv">
+
             <form id="mainScheduleForm">
+                <div id="form">
+                    <text></text>
+                </div>
+
                 <div id="scheduleInfoDiv">
-                    <span id="timeSlotBlock">Time Block:
-                        <DatePicker id="datePicker" selected={startDate} onChange={date => setStartDate(date)} excludeTimes/>
+                    <span id="timeSlotBlock">Choose Day:
+                        <DatePicker id="datePicker" selected={startDate} onChange={date => setStartDate(date)} />
+                        <text id="chooseTime">Choose Time: </text>
                         <Dropdown id="dropDown" options={options} onChange={_onSelect} placeholder="Select an option" />
                     </span>
                     <br />
+                </div>
+                <div id="addDelButtons">
+                    <input type="button" id="addButton" value="+" onClick={addText}/>
+                    <input type="button" id="delButton" value="-" onClick={deleteText} />
                 </div>
             </form>
             <div id="buttonForm">
