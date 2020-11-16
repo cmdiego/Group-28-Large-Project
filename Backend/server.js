886fc425094b413102abd3bc0a90725658775350
Andrew Fauble <andrew.fauble@gmail.com>
2020-11-16 18:07:27 -0500

    updated with akash's changes

--- Backend/server.js
+++ Backend/server.js
@@ -2,28 +2,30 @@
 const express = require('express'); // middle-wear
 const cors = require('cors');
 const app = express();
-const mongoose = require('mongoose');
-
+const authRoutes = require("./routes/auth.routes");
+require("./database/connection.js");
 require('dotenv').config();
-
 
 app.use(cors());
 app.use(express.json());
-const port = process.env.PORT;
-const uri = process.env.ATLAS_URI;
 
-// connecting ot mongoose
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-// checking connection
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+const port = process.env.PORT;
+
+app.use('/auth', authRoutes);
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE, OPTIONS');
+    next();
 });
+
 
 // start Node + Express server on port
 app.listen(port, () => {
-  console.log('Server is running on port: ' + port);
+  console.log(`Server is running on port: + ${port}`);
 });
+
 
 //NOTE: Code from professor for connection to web server
 // app.use((req, res, next) =>
