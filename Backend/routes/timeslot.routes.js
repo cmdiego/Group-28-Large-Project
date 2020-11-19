1c3742393d60885522bafd615d1db1386c74a034
Andrew Fauble <andrew.fauble@gmail.com>
2020-11-18 21:27:08 -0500

    added comments about how this file should look

--- Backend/routes/timeslot.routes.js
+++ Backend/routes/timeslot.routes.js
@@ -2,6 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 
+/*
+timeslot({
+  course: strings (course name: COP3400)
+  Tutor({
+    firstName: string
+    lastName: string
+  })
+  date: Date
+})
+
+ */
+
 
 router.post('/add', addTimeslot);
 
