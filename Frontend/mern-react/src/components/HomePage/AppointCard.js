3344796eec2ae9da58bafc2c16dea1ef8f0d87c5
EddBoyo <edd.baez42@knights.ucf.edu>
2020-11-20 18:55:21 -0500

    updated Schedule Builder and Home Page

---
+++ Frontend/mern-react/src/components/HomePage/AppointCard.js
@@ -0,0 +1,125 @@
+import React from 'react';
+
+/*
+ var res = [
+     {
+         Course: ,
+         Tutor: {
+             firstName: ,
+             lastName: 
+         },
+         Student: {
+             firstName: ,
+             lastName:
+         }
+         Date: whole date Object
+            but will have to format date and time out of it
+            getHours, and something else
+     },{}
+ ]
+*/
+
+function CourseInfo(props) {
+    return (
+        <text>{'Course: ' + props.Course}</text>
+    );
+}
+
+// gonna pass prop in as props.Tutor
+function TutorInfo(props) {
+    return (
+        <text>{'Tutor: ' + props.Tutor.firstName + ' ' + props.Tutor.lastName}</text>
+    );
+}
+
+function StudentInfo(props) {
+    return (
+        <text>{'Student: ' + props.Student.firstName + ' ' + props.Student.lastName}</text>
+    );
+
+}
+
+// takes date object and formats to mm/dd/yyyy format to output or whatever the local date structure is
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+function DateInfo(props) {
+    return (
+    <text>{'Date: ' + formatDate(props.Date)}</text>
+    );
+}
+
+function formatTime(date) {
+    var dateStuff = date.getHours();
+    var format;
+    // go in if its midnight
+    if (dateStuff === 0)
+    {
+        format = (dateStuff + 12) + ' am';
+    }
+    else if (dateStuff < 12)
+    {
+        format = dateStuff + ' am';
+    }
+    else if (dateStuff > 12)
+    {
+        format = (dateStuff - 12) + ' pm';
+    }
+    // reach here if it is 12 pm
+    else
+    {
+        format = dateStuff + ' pm';
+    }
+    return format;
+}
+
+function TimeInfo(props) {
+    return (
+        <text>{'Time: ' + formatTime(props.Date)}</text>
+    );
+}
+
+class AppointCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.info = props.info;
+        this.isStudent = props.isStudent;
+        this.state = {isDisplayed: true};
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    }
+    handleDeleteClick() {
+        var choice = window.confirm("Are you sure you want to cancel this Appointment?");
+        if (choice)
+        {
+            this.setState({isDisplayed: false});
+        }
+    }
+
+    render() {
+        const isDisplayed = this.state.isDisplayed;
+        var isStudent = this.isStudent;
+        var stuff = this.info;
+        if (isDisplayed)
+        {
+            return (
+                <div>
+                    <CourseInfo Course={stuff.Course} />
+                    <DateInfo Date={stuff.Date} />
+                    <br />
+                    {isStudent ? <TutorInfo Tutor={stuff.Tutor} /> : <StudentInfo Student={stuff.Student} />}
+                    <TimeInfo Date={stuff.Date} />
+                    <br />
+                    <button onClick={this.handleDeleteClick}>X</button>
+                </div>
+            );
+        }
+        else{
+            return null;
+        }
+    }
+    
+}
+
+
+export default AppointCard;
--- Frontend/mern-react/src/components/HomePage/Home.js
+++ Frontend/mern-react/src/components/HomePage/Home.js
@@ -1,6 +1,53 @@
 import React, { useState } from 'react';
+import AppointCard from './AppointCard';
 import './Home.css';
-
+/*
+function Thingerino(props) {
+    var mockfirstName = 'Brent';
+    var mocklastName = 'Smith';
+    var mockCode = 'COP';
+    var mockNumber = '3402';
+    var mockDate = '11/10';
+    var mockTime = '9:45';
+    var isStudent = true;
+    var show = true;
+    const delet = async event =>
+    {
+        event.preventDefault();
+        show = false;
+    }
+    if (show)
+    {
+       return (
+        <div id="wrapAround">
+            <div id="mockCard">
+                <span id="coursePart">
+                    <text>Course: </text>
+                    <text>{mockCode + ' ' + mockNumber}</text>
+                </span>
+                <span id="datePart">
+                    <text>Date: </text>
+                    <text>{mockDate}</text>
+                </span>
+                <span id="tutorPart">
+                    <text>Tutor: </text>
+                    <text>{mockfirstName + ' ' + mocklastName}</text>
+                </span>
+                <span id="timePart">
+                    <text>Time: </text>
+                    <text>{mockTime}</text>
+                </span>
+            </div>
+            <button id="deleteCardButton" onClick={delet}>X</button>
+        </div>
+       );
+    }
+    else
+    {
+        return null;
+    }
+}
+*/
 function Home () {
     /*will be determined with data from user but for now set to student homepage look*/
     var mockfirstName = 'Brent';
@@ -10,6 +57,22 @@ function Home () {
     var mockDate = '11/10';
     var mockTime = '9:45';
     var isStudent = true;
+    var testInfo = [
+        {
+            Course: 'COP4600',
+            Tutor: 
+            {
+                firstName: 'Jeremy',
+                lastName: 'Elbertson'
+            },
+            Student: 
+            {
+                firstName: 'John',
+                lastName: 'Johnson'
+            },
+            Date: new Date()
+        }
+    ];
     const searchProcess = async event =>
     {
         event.preventDefault();
@@ -33,7 +96,7 @@ function Home () {
         event.preventDefault();
         alert("delete card/ cancel appointment");
     };
-
+    
     return (
         <div id="HomepageDiv">
             <input type="button" id="profileButton" class="button" value="Profile" onClick={profileProcess} />
@@ -60,6 +123,13 @@ function Home () {
                 </div>
                 <button id="deleteCardButton" onClick={deleteCardProcess}>X</button>
             </div>
+            <div>
+                {testInfo.map(infoStuff => (
+                    <AppointCard info={infoStuff} isStudent={true} />
+                ))}
+            
+            </div>
+            {/*
             <div id="wrapAround">
                 <div id="mockCard">
                     <span id="coursePart">
@@ -81,6 +151,7 @@ function Home () {
                 </div>
                 <button id="deleteCardButton" onClick={deleteCardProcess}>X</button>
             </div>
+            */}
         </div>
     );
 }
