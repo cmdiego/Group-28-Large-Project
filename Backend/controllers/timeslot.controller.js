28b24c1b4a69c6352270d09a8c8facb7ee74ff89
Andrew Fauble <andrew.fauble@gmail.com>
2020-11-22 11:55:19 -0500

    created a timeslot.controller file and added a few function
    addTimeslot- for creating a new timeslot object
    removeTimeslot- when an appointment is canceled and some fields should 
    be made blank again
    setTimeslot- when a student accepts an appointment and new data should 
    enter fields

---
+++ Backend/controllers/timeslot.controller.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const Appointment = require('../models/appointment');
+const User = require('../models/user');
+
+
+export.addTimeslot = async function(req, res) {
+
+  const user = req.user;
+  const date = req.date;
+
+  // TODO set the correct time of day of the date object based on whatever is passed to us to show the time of day
+
+  let newTimeslot = new Appointment({
+    "tutorEmail": user.email,
+    "date": date
+  });
+  newTimeslot.save(err, success) => {
+    if(err) {
+      console.log("Error creating a timeslot object in database\n");
+      return res.status(400).json({error: "Error saving timeSlot to db"});
+    }
+    res.json({message: "timeSlot creation successful"});
+  }
+}
+
+
+export.removeTimeslot = async function(req, res) {
+
+  // TODO: are we tracking the ID of the timeslot objects?
+  // we need to have a way to identify them from the frontend
+  const id = req.id; // or whatever will go here
+
+  // also not sure what the value in the db for the identifier will be called
+  Appointment.findOne({id: id}, function(err, timeSlot){
+    if(err){
+      console.log("didn't find a matching appointment with id");
+      return res.status(400).json({error: "no matching appointment"});
+    }
+    timeSlot.studentEmail = "";
+    timeSlot.course = "";
+    // 'timeSlot.date' connant be set to null
+    // but if we always check 'studentEmail' != "" we shouln't have any issues
+    timeSlot.save(err, success) => {
+      if(err)
+      {
+        console.log("Error removing timeslot");
+        return res.status(400).json({error: "did not remove timeslot correctly"});
+      }
+      res.json({message: "timeslot removed successfully"});
+    };
+  });
+}
+
+
+export.setTimeslot = async function(req, res) {
+
+  const user = req.user;
+  // again not sure how we are identifying these appointment objects from the frontend
+  const id = req.id;
+  // also not sure how the course id will be sent
+  const course = req.course;
+
+  // again not sure how the identifier is stored in the db
+  Appointment.findOne({id: id}, function(err, timeSlot){
+    if(err){
+      console.log("didn't find a matching appointment with id");
+      return res.status(400).json({error: "no matching appointment"});
+    }
+    timeSlot.studentEmail = user.email;
+    timeSlot.course = course;
+    timeSlot.save(err, success) => {
+      if(err)
+      {
+        console.log("Error in timeslot setup");
+        return res.status(400).json({error: "did not setup timeslot correctly"});
+      }
+      res.json({message: "timeslot setup successfully"});
+    };
+  });
+}
