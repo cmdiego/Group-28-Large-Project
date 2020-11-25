9e5580910a7cfe4de67b2ed1dde0e68b0f5dd2f6
JainavPatel <54753556+JainavPatel@users.noreply.github.com>
2020-11-25 08:14:57 -0500

    Add files via upload

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 //***************************User COLLECTION *********************/
 //*** defining schemas ********/
 const userSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId, 
     email: {
         type: String,
         trim: true,
@@ -12,28 +11,23 @@ const userSchema = new mongoose.Schema({
         lowercase: true 
     }, 
     password : String,
-
     isStudent: Boolean, 
     isTutor: Boolean,
-
     firstName: {
         type: String,
         trim: true,
         required: true, 
-        unique: true,
     }, 
     lastName: {
         type: String,
         trim: true,
         required: true, 
-        unique: true,
     }, 
     schoolName: {
         type: String,
         required: true, 
     }, 
     bioBox: String,
-
     courses: {
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'Courses'
