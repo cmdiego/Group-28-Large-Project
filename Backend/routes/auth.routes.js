07c544b600d287a8fe18d0c9c67424a41c96d367
JainavPatel <54753556+JainavPatel@users.noreply.github.com>
2020-11-25 08:15:58 -0500

    Add files via upload

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,12 +1,14 @@
-const express = require("express");
-const router = express.Router();
-//Import controller of signup
-const {signup, signin, activateAccount, courseSetup, getUserInfo} = require("../controllers/auth.controller");
+const express = require("express"); 
+const router = express.Router(); 
+//Import controller of signup 
+const {signup, signin, requestPassword, resetPassword, activateAccount, courseSetup, getUserInfo} = require("../controllers/auth.controller");
 
 //API Endpoint for localhost:5000/signup directed to the signup method
-router.post('/signup', signup);
-router.post('/signin', signin);
-router.post('/email-activate', activateAccount);
+router.post('/signup', signup); 
+router.post('/signin', signin); 
+router.post('/request-password', requestPassword); 
+router.post('/reset-password', resetPassword); 
+router.post('/email-activate', activateAccount); 
 router.post('/addcourse', courseSetup);
 router.get('/userinfo', getUserInfo);
 //Export features for other to transfer
