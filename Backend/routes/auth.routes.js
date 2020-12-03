c69771fff8b88fe4ec3b6358e9f54786eb11e91d
Akash Samlal <triforcemaster345@gmail.com>
2020-12-03 16:27:41 -0500

    Update Password in Profile Page

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const express = require("express"); 
 const router = express.Router(); 
 //Import controller of signup 
-const {signup, signin, requestPassword, authenticateToken, resetPassword, activateAccount, courseSetup, getUserInfo} = require("../controllers/auth.controller");
+const {signup, signin, requestPassword, authenticateToken, resetPassword, activateAccount, courseSetup, getUserInfo, 
+    modifyBioBox, changePassword } = require("../controllers/auth.controller");
 
 //API Endpoint for localhost:5000/signup directed to the signup method
 router.post('/signup', signup); 
@@ -11,5 +12,7 @@ router.post('/reset-password', resetPassword);
 router.post('/email-activate', activateAccount); 
 router.post('/addcourse', authenticateToken, courseSetup);
 router.get('/userinfo', authenticateToken, getUserInfo);
+router.post('/bioBox', authenticateToken, modifyBioBox);
+router.post('/changePassword', authenticateToken, changePassword);
 //Export features for other to transfer
 module.exports = router; 
