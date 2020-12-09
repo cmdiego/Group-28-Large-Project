ff2aa3f16f19e8d78c9eabe64d647a7fd85c8be1
JainavPatel <54753556+JainavPatel@users.noreply.github.com>
2020-12-09 14:44:29 -0500

    Add files via upload

--- Frontend/mern-react/src/components/SigninPage/Signin.js
+++ Frontend/mern-react/src/components/SigninPage/Signin.js
@@ -3,10 +3,24 @@ import './SignIn.css';
 import otLogo from '../../otLogo.png';
 const axios = require('axios');
 
-var alreadyPressed == false
+var alreadyPressed = false;
 
 function Signin()
 {
+    //Heroku deployment 
+    const app_name = 'opentutor'
+    function buildPath(route)
+    {
+        if(process.env.NODE_ENV === 'production')
+        {
+            return 'https://' + app_name + '.herokuapp.com/' + route;
+        }
+        else
+        {
+            return 'https://localhost:5000/' + route;
+        }
+    }
+
     var signinEmail;
     var signinPassword;
     const [message,setMessage] = useState('');
@@ -17,39 +31,38 @@ function Signin()
     const signinProcess = async event =>
     {
         event.preventDefault();
-
+        
         let req = {
-            email: signinEmail.value,
+            email: signinEmail.value, 
             password: signinPassword.value
         }
 
         if (state.button === 1)
         {
-            axios.post('http://localhost:5000/auth/signin', req)
+            axios.post(buildPath('auth/signin'), req)
             .then(function(resp) {
                 const { accessToken } = resp.data;
                 localStorage.setItem('jwtToken', accessToken);
-                const status = resp.status;
+                const status = resp.status; 
                 if(status == 200)
                      window.location = '/HomePage';
-                else if(status == 401 || status == 402)
+                else if(status == 201 || status == 202)
                 {
-                  if (!alreadyPressed)
-                  {
-                      var newdiv = document.createElement('div');
-                      newdiv.innerHTML = ('<div id= "confirmAppend"><span="inner">Incorrect password or email<br /></span></div>');
-                      document.getElementById("theForm").appendChild(newdiv);
-                      alreadyPressed = true;
-                  }
+                    if(!alreadyPressed)
+                    {
+                        var newdiv = document.createElement('div');
+                        newdiv.innerHTML = ('<div id= "confirmAppend"><span="inner">Incorrect password or email<br /></span></div>');
+                        document.getElementById("theForm").appendChild(newdiv);
+                        alreadyPressed = true;
+                    }
                 }
-
             })
                 .catch(err => {
-                    console.log(err);
+                    console.log(err); 
                 })
-
+                
                 axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem('jwtToken');
-            //alert('signing in ' + signinEmail.value + ' ' + signinPassword.value);
+            //alert('signing in ' + signinEmail.value + ' ' + signinPassword.value//
         }
         else if (state.button === 2)
         {
@@ -63,21 +76,21 @@ function Signin()
 
     return (
         <div id="signinDiv">
-            <img class = "img-rounded" src = {otLogo} alt ="otLogo"/>
+            <img class = "img-rounded" src = {otLogo} alt ="otLogo"/>    
             <br></br>
             <form onSubmit={signinProcess} id = "formID">
                 <span id="inner-title">Sign In</span><br />
                 <input type="email" id="signinEmail" placeholder="Type Email" ref={ (c) => signinEmail = c} /><br />
                 <input type="password" id="signinPassword" placeholder="Type Password" ref={ (c) => signinPassword = c} /><br />
                 <input type="submit" id="signinButton" class="buttons" value="Sign in" onClick={() => state.button = 1} />
+                <div id="confirmNotify">
+                     <form id="theForm">
+                         <text></text>
+                    </form>
+                </div>
                 <label>Dont have an account?<input type="submit" id="defferSignIn" class="buttons" value="Sign up" onClick={() => state.button = 2} /></label>
                 <input type="submit" id="defferSignIn" class="buttons" value="Forgot Password?" onClick={() => state.button = 3} />
             </form>
-            <div id="confirmNotify">
-            <form id="theForm">
-                <text></text>
-            </form>
-        </div>
     <span id="signinResult">{message}</span>
         </div>
     );
